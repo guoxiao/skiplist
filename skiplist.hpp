b31e947d147a1b2f58130d2ff9039f5d23d59cf4
Guo Xiao <guoxiao08@gmail.com>
2015-12-26 00:57:49 +0800

    Initial Commit

---
+++ skiplist.hpp
@@ -0,0 +1,278 @@
+#pragma once
+
+#include <vector>
+#include <assert.h>
+#include <cstdlib>
+#include <iostream>
+#include <stdexcept>
+
+namespace guoxiao {
+namespace skiplist {
+
+template <typename KeyType, typename ValueType>
+struct SkipNode {
+  KeyType key;
+  ValueType value;
+  size_t level;
+  std::vector<SkipNode *> next;
+  SkipNode() : level(0), next(1) {}
+  SkipNode(SkipNode &&rhs) : level(rhs.level), next(std::move(rhs.next)) {}
+
+  SkipNode(const SkipNode &) = delete;
+  SkipNode &operator=(const SkipNode &) = delete;
+};
+
+template <typename T>
+class Iterator {
+public:
+  Iterator() : ptr(nullptr){};
+  Iterator(T *begin) : ptr(begin){};
+  Iterator operator=(T *begin) { ptr = begin; }
+
+  Iterator &operator++() {
+    *this = Iterator(ptr->next[0]);
+    return *this;
+  }
+
+  bool operator!=(const Iterator &rhs) const { return ptr != rhs.ptr; }
+
+  bool operator==(const Iterator &rhs) const { return ptr == rhs.ptr; }
+
+  bool operator==(const T* p) const { return ptr == p; }
+
+  bool operator!=(const T* p) const { return ptr != p; }
+
+  operator bool() { return ptr != nullptr; }
+
+  operator T *() { return ptr; }
+
+  T *operator->() { return ptr; }
+
+  T &operator*() { return *ptr; }
+
+  void deallocate() { delete ptr; }
+
+private:
+  T *ptr;
+};
+
+template <typename Key, typename T>
+class SkipList {
+public:
+  typedef Key key_type;
+  typedef T mapped_type;
+  typedef std::pair<Key, T> value_type;
+  typedef SkipNode<Key, T> node_type;
+  typedef Iterator<node_type> iterator;
+  typedef Iterator<node_type> const_iterator;
+
+  SkipList() : size_(0), level_(0), head_(new node_type()) {}
+
+  ~SkipList() {
+    iterator node = head_, temp;
+    while (node) {
+      temp = node;
+      node = node->next[0];
+      temp.deallocate();
+    }
+  }
+
+  // Move Ctor
+  SkipList(SkipList &&s) noexcept : size_(s.size),
+                                    level_(s.level_),
+                                    head_(s.head_) {
+    s->head_ = new node_type();
+    s->level_ = 0;
+    s->size_ = 0;
+  }
+
+  // Copy Ctor
+  SkipList(const SkipList &s)
+    : size_(s.size_), level_(s.level_), head_(new node_type()) {
+    iterator snode = s.head_, node = head_;
+    std::vector<iterator> last(level_ + 1);
+    head_->level = level_;
+    head_->next.resize(level_ + 1);
+    for (int i = level_; i >= 0; i--) {
+      last[i] = head_;
+    }
+    snode = snode->next[0];
+    while (snode) {
+      node = new node_type();
+      node->key = snode->key;
+      node->value = snode->value;
+      node->level = snode->level;
+      node->next.resize(snode->next.size());
+      for (int i = snode->level; i >= 0; i--) {
+        last[i]->next[i] = node;
+        last[i] = node;
+      }
+      snode = snode->next[0];
+    }
+  }
+
+  // Copy Assignment
+  SkipList &operator=(const SkipList &s) {
+    if (*this == s)
+      return *this;
+    SkipList tmp(s);
+    *this = std::move(tmp);
+  }
+
+  // Copy Assignment
+  SkipList &operator=(SkipList &&s) noexcept { *this = std::move(s); }
+
+  size_t size() const { return size_; }
+  bool empty() const { return size_ == 0; }
+  size_t level() const { return level_; }
+
+  iterator begin() noexcept { return head_; }
+  const_iterator begin() const noexcept { return head_; }
+
+  const_iterator cbegin() noexcept { return head_; }
+  const_iterator cend() const noexcept { return nullptr; }
+
+  iterator end() noexcept { return nullptr; }
+  const_iterator end() const noexcept { return nullptr; }
+
+  template <typename K, typename V>
+  iterator emplace(K &&key, V &&value) {
+    iterator node = head_;
+    std::vector<iterator> update(level_ + 1 + 1);
+    for (int i = level_; i >= 0; i--) {
+      assert(node->level >= i);
+      while (node->next[i] && node->next[i]->key < key) {
+        node = node->next[i];
+      }
+      update[i] = node;
+      if (node->next[i] && node->next[i]->key == key) {
+        throw std::runtime_error("conflict");
+      }
+    }
+
+    node_type *n = new node_type();
+    n->key = std::forward<K>(key);
+    n->value = std::forward<V>(value);
+
+    size_t level = getRandomLevel();
+    if (level > level_) {
+      level = level_ + 1;
+      head_->next.resize(level + 1);
+      head_->level = level;
+      update[level] = head_;
+      level_ = level;
+    }
+    n->next.resize(level + 1);
+    n->level = level;
+
+    for (int i = level; i >= 0; i--) {
+      if (update[i]) {
+        n->next[i] = update[i]->next[i];
+        update[i]->next[i] = n;
+      }
+    }
+    ++size_;
+    return n;
+  }
+
+  iterator insert(const value_type &value) {
+    return emplace(value.first, value.second);
+  }
+
+  iterator find(const key_type &key) {
+    iterator node = head_;
+    for (int i = level_; i >= 0; i--) {
+      while (node->next[i] && node->next[i]->key < key) {
+        node = node->next[i];
+      }
+      if (node->next[i] && node->next[i]->key == key) {
+        return node->next[i];
+      }
+    }
+    return nullptr;
+  }
+
+  void erase(const key_type &key) {
+    iterator node = head_;
+    std::vector<iterator> update(level_ + 1);
+
+    for (int i = level_; i >= 0; i--) {
+      while (node->next[i] && node->next[i]->key < key) {
+        node = node->next[i];
+      }
+      if (node->next[i] && node->next[i]->key == key) {
+        update[i] = node;
+      }
+    }
+    assert(node->next[0]->key == key);
+
+    node = node->next[0];
+    for (int i = level_; i >= 0; i--) {
+      if (update[i]) {
+        assert(node == update[i]->next[i]);
+        update[i]->next[i] = node->next[i];
+      }
+    }
+
+    delete node;
+    --size_;
+
+    if (head_->next[level_] == nullptr) {
+      level_--;
+      head_->level = level_;
+      head_->next.resize(level_ + 1);
+    }
+  }
+
+  mapped_type &operator[](const key_type &key) {
+    iterator node = find(key);
+    if (node == nullptr) {
+      mapped_type value;
+      node = emplace(key, value);
+    }
+    return node->value;
+  }
+
+  mapped_type at(const key_type &key) {
+    iterator node = find(key);
+    if (node == nullptr) {
+      throw std::out_of_range("skiplist::at");
+    }
+    return node->value;
+  }
+
+  const mapped_type at(const key_type &key) const {
+    iterator node = find(key);
+    if (node == nullptr) {
+      throw std::out_of_range("skiplist::at");
+    }
+    return node->value;
+  }
+
+  void dump() {
+    for (int i = level_; i >= 0; i--) {
+      std::cout << "====== level " << i << std::endl;
+      auto node = head_;
+      while (node->next[i]) {
+        std::cout << node->next[i]->key << " : " << node->next[i]->value
+                  << std::endl;
+        node = node->next[i];
+      }
+    }
+  }
+
+private:
+  size_t size_;
+  size_t level_;
+  iterator head_;
+  static size_t getRandomLevel() {
+    size_t level = 0;
+    while (rand() % 2) {
+      ++level;
+    }
+    return level;
+  }
+};
+
+} // namespace skiplist
+} // namespace guoxiao
